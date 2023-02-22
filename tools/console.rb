1e255f0f773b84c06704acd4fb0a925d8e600805
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-02-22 21:26:48 +0300

    completed

--- tools/console.rb
+++ tools/console.rb
@@ -6,20 +6,19 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-### Author class initialization test
-sylvia = Author.new("Sylvia")
-taner = Author.new("Taner")
-mongina = Author.new("Mongina")
 
-### Magazine class initialization test
-new_york_times = Magazine.new("New York Times", "News")
-forbes = Magazine.new("Forbes", "Entertainment")
-top_forty_under_forty = Magazine.new("Top 40 Under 40", "Inspirational")
+article1=Author.new("Author1")
+magazine1=Magazine.new("Magazine1","Category1")
+magazine2=Magazine.new("Magazine2","Category1")
+magazine3=Magazine.new("Magazine3","Category2")
+artic1=Article.new(article1,magazine1,"Title1")
+artic2=Article.new(article1,magazine1,"Title2")
+article1.add_article(magazine1,"Title3")
+article1.add_article(magazine1,"Title6")
+article1.add_article(magazine1,"Title7")
+article1.add_article(magazine2,"Title4")
+article1.add_article(magazine3,"Title5")
 
-### Article class initialization test
-sylvia.add_article(entertainment, "5 Facts About Me You Didn't Know")
-taner.add_article(education, "The Ultimate Developers Guide")
-mongina.add_article(health, "Why you need to start working out")
 
 ### DO NOT REMOVE THIS
 binding.pry
